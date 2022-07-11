1ce23a15d5afaace1dab8090b13f12ab16a5bfb9
Mamosebetsi Kekae <mkekae@gmail.com>
2022-07-11 13:26:52 -0700

    finished the hero and footer section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
 	content: ["./*.html"],
 	theme: {
-		extend: {},
+		extend: {
+			fontSize: {
+				sm: ["14px"],
+			},
+			colors: {
+				starGreen: "#D4E9E2",
+				darkGreen: "#1E3932",
+				lightText: "rgba(0,0,0, .58)",
+				titleText: "rgba(0,0,0, .87)",
+			},
+		},
 	},
 	plugins: [],
 };
